f4e2eb5a9384cc643e1ce37f168666e2d559efbc
Felix <felix_1999@live.no>
2024-02-22 21:36:59 +0100

    :fire: Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
-/* import { useState } from "react"; */
 import "./App.css";
 
 function App() {
-	/* const [count, setCount] = useState(0); */
-
 	return (
 		<>
-			{/* <div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p> */}
-
 			<div className="fixed grid grid-rows-1 grid-cols-6 w-96 h-[34rem] bottom-0 right-0 bg-black border-solid border-t border-l border-[#ffbb82] rounded-tl-md">
 				<div className="p-2 col-span-4 flex flex-col justify-between w-full">
 					<div className="h-48  flex gap-2 z-10 flex-col overflow-y-auto overflow-x-hidden relative p-1">
