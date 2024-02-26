328aca1a61a827b5dbbf875662cb398505f20b29
Felix <felix_1999@live.no>
2024-02-26 22:06:23 +0100

    :lipstick: Make components

---
+++ src/components/AvatarModual.tsx
@@ -0,0 +1,10 @@
+export function AvatarModual() {
+	return (
+		<div className="h-full w-full flex items-end col-span-2 flex-col justify-between">
+			<div className="bg-gray-700 p-1 rounded-bl-md">
+				<p>Close X</p>
+			</div>
+			<div className="h-[26rem] w-full bg-avatar bg-cover bg-no-repeat bg-bottom"></div>
+		</div>
+	);
+}
